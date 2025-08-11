15dedbedae305cfc55c33ad343fb2c03364d6d6b
dlsxj101 <dlsxj101@naver.com>
2025-08-11 07:27:11 -0700

    [Silver I] Title: 쉬운 계단 수, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -12,23 +12,19 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine().trim());
 
-        long memorized[][] = new long[101][10];
+        long memorized[][] = new long[N+1][10];
 
         for(int i = 1; i < 10; i++){
             memorized[1][i] = 1;
         }
 
-        long cnt[] = new long[101];
-        cnt[1] = 1;
-        for(int i = 2; i < 101; i++){
+        for(int i = 2; i <= N; i++){
             for(int j = 0; j < 10; j++){
                 if(j == 0){
                     memorized[i][j] = memorized[i-1][1] % MOD;
-                    cnt[i] = (cnt[i] + memorized[i][j]) % MOD;
                 }
                 else if(j == 9){
                     memorized[i][j] = memorized[i-1][8] % MOD;
-                    cnt[i] = (cnt[i] + memorized[i][j]) % MOD;
                 }
                 else{
                     memorized[i][j] = (memorized[i-1][j-1] + memorized[i-1][j+1]) % MOD;
@@ -36,18 +32,12 @@ public class Main {
             }
         }
 
-        long dp[] = new long[N + 1];
+        long ans = 0;
 
-        dp[1] = 9;
-        for(int i = 2; i <= N; i++){
-            long two = (dp[i - 1] * 2) % MOD;        // 곱셈도 즉시 모듈러
-            long val = (two - cnt[i - 1]) % MOD;     // 뺄셈 후 음수 가능
-            if (val < 0) val += MOD;                 // 음수 보정
-            dp[i] = val;
+        for(int i = 0; i < 10; i++){
+            ans = (ans + memorized[N][i]) % MOD;
         }
 
-        System.out.println(dp[N] % MOD);
-
-
+        System.out.println(ans);
     }
 }
