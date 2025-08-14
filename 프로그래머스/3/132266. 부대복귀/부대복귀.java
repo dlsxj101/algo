0e7107bd4bbddd01d5cae2f4e4f88ff0c71509b6
dlsxj101 <dlsxj101@naver.com>
2025-08-14 05:34:30 -0700

    [level 3] Title: 부대복귀, Time: 188.21 ms, Memory: 170 MB -BaekjoonHub

---
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        List<Integer> adjList[] = new List[n + 1];
+        for(int i = 1; i <= n; i++){
+            adjList[i] = new ArrayList<>();
+        }
+        
+        for(int i = 0; i < roads.length; i++){
+            adjList[roads[i][0]].add(roads[i][1]);
+            adjList[roads[i][1]].add(roads[i][0]);
+        }
+        
+        int dist[] = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
+        boolean visited[] = new boolean[n+1];
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        dist[destination] = 0;
+        q.add(destination);
+        
+        while(!q.isEmpty()){
+            int now = q.poll();
+            
+            // if(visited[now]) continue;
+            
+            for(int next : adjList[now]){
+                if(dist[next] > dist[now] + 1){
+                    dist[next] = dist[now] + 1;
+                    q.add(next);
+                    visited[next] = true;
+                }
+            }
+        }
+        
+        // System.out.println(Arrays.toString(dist));
+        
+        int[] answer = new int[sources.length];
+        
+        for(int i = 0; i < sources.length; i++){
+            if(dist[sources[i]] == Integer.MAX_VALUE) {
+                answer[i] = -1;
+            }
+            else answer[i] = dist[sources[i]];
+        }
+        return answer;
+    }
+}
