7127b38da3374c091430e72894137843286e67ee
dlsxj101 <dlsxj101@naver.com>
2025-08-16 05:58:47 -0700

    [level 3] Title: 합승 택시 요금, Time: 99.04 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    
+    static int N;
+    static int dist[][];
+    
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        
+        N = n;
+        dist = new int[n + 1][n + 1];
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == j) continue;
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        
+        for(int i = 0; i < fares.length; i++){
+            dist[fares[i][0]][fares[i][1]] = fares[i][2];
+            dist[fares[i][1]][fares[i][0]] = fares[i][2];
+        }
+        
+        for(int k = 1; k <= n; k++){
+            for(int i = 1; i <= n; i++){
+                if(dist[i][k] == Integer.MAX_VALUE) continue;
+                for(int j = 1; j <= n; j++){
+                    if(dist[k][j] == Integer.MAX_VALUE) continue;
+                    if(dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+        
+        // for(int i = 1; i <= n; i++){
+        //     System.out.println(Arrays.toString(dist[i]));
+        // }
+        
+        int answer = Integer.MAX_VALUE;
+        
+        for(int i = 1; i <= n; i++){
+            answer = Math.min(answer, dist[s][i] + dist[i][a] + dist[i][b]);
+        }
+        
+        return answer;
+    }
+}
